3bc7a1cf8799ba593954ae15258dc60de0edb07f
godzidcrypto <godzidcrypto@gmail.com>
2022-09-11 02:21:47 +0800

    updated polls to be created per project instead of array

--- controllers/pollsController.js
+++ controllers/pollsController.js
@@ -2,100 +2,86 @@ const Poll = require("../models/pollsModel");
 const mongoose = require("mongoose");
 
 const getPolls = async (req, res) => {
-  //   res.status(200).json([
-  //     {
-  //       date: "today",
-  //       mints: [
-  //         {
-  //           title: "Degen Fat Cats",
-  //           yes: 10,
-  //           no: 2,
-  //         },
-  //         {
-  //           title: "Degen Fat Cats",
-  //           yes: 10,
-  //           no: 2,
-  //         },
-  //         {
-  //           title: "Degen Fat Cats",
-  //           yes: 10,
-  //           no: 2,
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       date: "today",
-  //       mints: [
-  //         {
-  //           title: "Degen Fat Cats",
-  //           yes: 10,
-  //           no: 2,
-  //         },
-  //         {
-  //           title: "Degen Fat Cats",
-  //           yes: 10,
-  //           no: 2,
-  //         },
-  //         {
-  //           title: "Degen Fat Cats",
-  //           yes: 10,
-  //           no: 2,
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       date: "today",
-  //       mints: [
-  //         {
-  //           title: "Degen Fat Cats",
-  //           yes: 10,
-  //           no: 2,
-  //         },
-  //         {
-  //           title: "Degen Fat Cats",
-  //           yes: 10,
-  //           no: 2,
-  //         },
-  //         {
-  //           title: "Degen Fat Cats",
-  //           yes: 10,
-  //           no: 2,
-  //         },
-  //       ],
-  //     },
-  //   ]);
-
   const polls = await Poll.find({}).sort({ createdAt: -1 });
 
   res.status(200).json(polls);
 };
 
 const createPoll = async (req, res) => {
-  const { date, mints } = req.body;
+  const {
+    date,
+    time,
+    wlTime,
+    name,
+    twitter,
+    discord,
+    website,
+    mintPrice,
+    wlMintPrice,
+    quantity,
+    lauren,
+    hotsauce,
+    dagzen,
+    yes,
+    voters,
+  } = req.body;
 
   try {
     const poll = await Poll.create({
       date,
-      mints,
+      time,
+      wlTime,
+      name,
+      twitter,
+      discord,
+      website,
+      mintPrice,
+      wlMintPrice,
+      quantity,
+      lauren,
+      hotsauce,
+      dagzen,
+      yes,
+      voters,
     });
     res.status(200).json(poll);
   } catch (error) {
+    console.log(error.message);
     res.status(400).json({ error: error.message });
   }
 };
 
 const updateVote = async (req, res) => {
-  const { id } = req.params;
-  const { projectId, yes, no, email } = req.body;
+  //   const { id } = req.params;
+  //   const { date, mints } = req.body;
+  // will only run if projectId does not exist
+  // it updates the poll based on the date
+  // it adds a new projectId to the mints array
+
+  // code for updating votes
+  // will only run if projectId exists
+  const { id, vote, email } = req.body;
+
+  console.log(vote);
 
   try {
-    await Poll.updateOne(
-      { "mints._id": projectId },
-      {
-        $inc: { "mints.$.yes": yes ? 1 : 0, "mints.$.no": no ? 1 : 0 },
-        $push: { "mints.$.voters": { email: email, vote: yes ? "Yes" : "No" } },
-      }
-    );
+    if (vote) {
+      await Poll.updateOne(
+        { _id: id },
+        {
+          $inc: { yes: 1 },
+          $push: { voters: { email: email } },
+        }
+      );
+    } else if (!vote) {
+      await Poll.updateOne(
+        { _id: id },
+        {
+          $inc: { yes: -1 },
+          $pull: { voters: { email: email } },
+        }
+      );
+    }
   } catch (error) {
     console.log(error.message);
     res.status(400).json({ error: error.message });
--- models/pollsModel.js
+++ models/pollsModel.js
@@ -7,14 +7,50 @@ const pollSchema = new Schema({
     type: Date,
     required: true,
   },
-  mints: [
-    {
-      project: String,
-      yes: { type: Number, default: 0 },
-      no: { type: Number, default: 0 },
-      voters: { type: [{ email: String, vote: String }], default: [] },
-    },
-  ],
+  time: {
+    type: String,
+    required: true,
+  },
+  wlTime: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  twitter: {
+    type: String,
+    required: true,
+  },
+  discord: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+  },
+  mintPrice: {
+    type: String,
+    required: true,
+  },
+  wlMintPrice: {
+    type: String,
+  },
+  quantity: {
+    type: String,
+    required: true,
+  },
+  lauren: {
+    type: Boolean,
+  },
+  hotsauce: {
+    type: Boolean,
+  },
+  dagzen: {
+    type: Boolean,
+  },
+  yes: { type: Number, default: 0 },
+  voters: { type: [{ email: String, vote: String }], default: [] },
 });
 
 module.exports = mongoose.model("Poll", pollSchema);
